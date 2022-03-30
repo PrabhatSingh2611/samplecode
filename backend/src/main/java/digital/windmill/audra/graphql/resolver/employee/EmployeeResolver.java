b7f9f4275f087d6f263e1a59992a955453fc9635
abhishek kumar <abhishek.kumar@windmill.digital>
2022-03-30 12:28:34 +0530

    VA-75 : Implement employee query

--- backend/src/main/java/digital/windmill/audra/graphql/resolver/employee/EmployeeResolver.java
+++ backend/src/main/java/digital/windmill/audra/graphql/resolver/employee/EmployeeResolver.java
@@ -2,6 +2,7 @@ package digital.windmill.audra.graphql.resolver.employee;
 
 import digital.windmill.audra.graphql.type.ConnectionPayload;
 import digital.windmill.audra.graphql.type.Employee;
+import digital.windmill.audra.graphql.type.input.EmployeeInput;
 import digital.windmill.audra.graphql.utils.ConnectionUtils;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Component;
@@ -15,7 +16,13 @@ import java.util.List;
 @AllArgsConstructor
 public class EmployeeResolver implements GraphQLQueryResolver {
 
+/*
     public ConnectionPayload<Employee> employees() {
+        return ConnectionUtils.buildPayload(new PageImpl<>(List.of()));
+    }
+*/
+
+    public ConnectionPayload<Employee> employees(EmployeeInput input) {
         return ConnectionUtils.buildPayload(new PageImpl<>(List.of()));
     }
 }
