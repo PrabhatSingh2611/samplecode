46c916c54f394cf49a48c392ca0acab44ebfe1fe
wmssravi <ravi.raj@windmill.digital>
2022-04-05 15:39:03 +0530

    uses removed from AssetTypeMapper

--- backend/src/main/java/digital/windmill/audra/graphql/mapper/AssetTypeMapper.java
+++ backend/src/main/java/digital/windmill/audra/graphql/mapper/AssetTypeMapper.java
@@ -5,7 +5,7 @@ import org.mapstruct.Mapper;
 import digital.windmill.audra.dao.entity.AssetTypeEntity;
 import digital.windmill.audra.graphql.type.AssetType;
 
-@Mapper(componentModel = "spring",uses = {DateTimeMapper.class, EmployeeMapper.class})
+@Mapper(componentModel = "spring")
 public interface AssetTypeMapper {
 
     AssetType map(AssetTypeEntity entity);
