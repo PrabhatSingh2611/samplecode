6e0f4183725afeda61dc35c199bb524937bfee7c
Sergiu Chiriac <sergiuchiriacei@gmail.com>
2022-04-08 14:52:32 +0100

    VA-89 - revert imports from Wildcard imports(*)

--- backend/src/main/java/digital/windmill/audra/dao/entity/AssetEntity.java
+++ backend/src/main/java/digital/windmill/audra/dao/entity/AssetEntity.java
@@ -3,7 +3,14 @@ package digital.windmill.audra.dao.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import java.time.Instant;
 import java.util.UUID;
 
