588b348ffc1c6308f1c599985770dca0a964e57e
wmssravi <ravi.raj@windmill.digital>
2022-04-17 15:49:07 +0530

    updated with tests

--- backend/src/main/java/digital/windmill/audra/graphql/facade/impl/AssetFacadeImpl.java
+++ backend/src/main/java/digital/windmill/audra/graphql/facade/impl/AssetFacadeImpl.java
@@ -3,7 +3,7 @@ package digital.windmill.audra.graphql.facade.impl;
 import digital.windmill.audra.graphql.facade.AssetFacade;
 import digital.windmill.audra.graphql.type.Asset;
 import digital.windmill.audra.graphql.type.input.AssetsInput;
-import digital.windmill.audra.service.impl.AssetServiceImpl;
+import digital.windmill.audra.service.AssetService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -15,17 +15,17 @@ import java.util.UUID;
 @AllArgsConstructor
 public class AssetFacadeImpl implements AssetFacade {
 
-    private AssetServiceImpl assetServiceImpl;
+    private AssetService assetService;
 
     @Override
     @Transactional(readOnly = true)
     public Asset findAssetByUuid(UUID uuid) {
-        return assetServiceImpl.findAssetByUuid(uuid);
+        return assetService.findAssetByUuid(uuid);
     }
 
     @Override
     @Transactional(readOnly = true)
     public Page<Asset> findAllAssets(AssetsInput input) {
-        return assetServiceImpl.findAll(input);
+        return assetService.findAll(input);
     }
 }
