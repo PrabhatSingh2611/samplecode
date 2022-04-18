b5d7c1b66d0e3194f02dd8e095e75dadd6cbcf19
wmssravi <ravi.raj@windmill.digital>
2022-04-18 19:58:33 +0530

    Mapper implementation

--- backend/src/main/java/digital/windmill/audra/graphql/facade/LeaveRequestFacade.java
+++ backend/src/main/java/digital/windmill/audra/graphql/facade/LeaveRequestFacade.java
@@ -3,5 +3,9 @@ package digital.windmill.audra.graphql.facade;
 import digital.windmill.audra.graphql.type.LeaveRequest;
 
 public interface LeaveRequestFacade {
+    /** this method takes value and return LeaveRequest
+     * @param id takes Long ID as input
+     * @return a specific LeaveRequest
+     */
     LeaveRequest findLeaveRequestById(Long id);
 }
