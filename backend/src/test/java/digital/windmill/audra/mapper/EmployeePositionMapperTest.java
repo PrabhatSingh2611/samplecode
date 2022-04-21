4e6259bf02425d78592294fe5d9dff8e3886afa2
Sergiu Chiriac <sergiu.chiriac@windmill.digital>
2022-04-21 09:34:02 +0100

    VA-98: fixed wildcard import

--- backend/src/test/java/digital/windmill/audra/mapper/EmployeePositionMapperTest.java
+++ backend/src/test/java/digital/windmill/audra/mapper/EmployeePositionMapperTest.java
@@ -10,7 +10,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 
