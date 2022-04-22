732b15b4b8f3e630416c026aa9e9b8284f7e783f
Ivan Myronov <gmironovivan1992@gmail.com>
2022-04-22 11:42:34 +0300

    [AU] Scaffold documentation

---
+++ documentation/docs/frontend/intro.md
@@ -0,0 +1,33 @@
+---
+sidebar_position: 1
+---
+
+# Frontend Intro
+
+## Tech Stack:
+
+- [CRA](https://create-react-app.dev/) + React 18
+- [Craco](https://github.com/gsoft-inc/craco) for CRA Customization
+- [Webpack Module Federation](https://webpack.js.org/concepts/module-federation/) for Microfrontends
+- TypeScript for Type Checking
+- Nx/Turborepo for Monorepo **Need to choose one**
+- [Apollo Client](https://www.apollographql.com/docs/react/) for State Managment
+- [Jest](https://jestjs.io/) + [React testing library](https://testing-library.com/) for Testing
+- [TSDX](https://tsdx.io/) for WDX development
+- ESLint + Prettier for Linting and Code Formating
+- [Husky](https://typicode.github.io/husky/#/) Git hooks
+- [Bundle budgets](https://web.dev/codelab-setting-performance-budgets-with-webpack/) + [Unused exports](https://github.com/pzavolinsky/ts-unused-exports) for Bundle Optimization
+- [Why did you render](https://github.com/welldone-software/why-did-you-render) for React App Optimization
+- Storybook for WDX Components Documentation
+- [MUI](https://mui.com/) + [MUI Minimal Template](https://mui.com/store/items/minimal-dashboard/) for React UI and Design System
+- [Docozaurus](https://docusaurus.io/) for Documentation
+
+## Prerequisites:
+
+- Node.js v14
+- NPM
+- Access to Jira, BitBucket, Figma, Nexus, SonarQube, Trello **Provide Links**
+- Other Global Packages **Need to Discuss**
+- Editor Extensions **Need to Discuss**
+
+## How to start?
---
+++ documentation/docusaurus.config.js
@@ -0,0 +1,138 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Audra",
+  tagline:
+    "One source of truth, auto-deploy, MS Teams Notifications, easily to integrates in dev flow, everyone can update FE, BE, QA, DevOPs, PO, PM, Blog to keep retrospectives amd meeting notes",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://bitbucket.org/windmillgmbh/audra-platform/src/master/documentation",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://bitbucket.org/windmillgmbh/audra-platform/src/master/documentation",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Audra",
+        logo: {
+          alt: "Audra Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Tutorial",
+          },
+          {
+            type: "doc",
+            docId: "frontend/intro",
+            position: "left",
+            label: "Frontend",
+          },
+          {
+            type: "doc",
+            docId: "api/schema-conventions",
+            position: "left",
+            label: "API",
+          },
+          {
+            type: "doc",
+            docId: "project/intro",
+            position: "left",
+            label: "Project",
+          },
+          { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://bitbucket.org/windmillgmbh/audra-platform",
+            label: "BitBucket",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              },
+              {
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/docusaurus",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "BitBucket",
+                href: "https://bitbucket.org/windmillgmbh/audra-platform",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Windmill, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
