43f204ac35201a06a4bd646281dcda3015d0c216
Prabhat2611 <prabhat.singh@windmill.digital>
2022-05-03 19:42:57 +0530

    removed wildcard import

--- backend/src/test/java/digital/windmill/audra/service/LocationServiceTest.java
+++ backend/src/test/java/digital/windmill/audra/service/LocationServiceTest.java
@@ -15,7 +15,9 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
