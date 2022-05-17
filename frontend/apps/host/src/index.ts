5aa26f20cdaad0a1843d3e82deb4452c46e1cf2a
Ivan Myronov <gmironovivan1992@gmail.com>
2022-05-17 14:40:37 +0300

    [AU] Trigger CI/CD 3

--- frontend/apps/host/src/index.ts
+++ frontend/apps/host/src/index.ts
@@ -1,6 +1,6 @@
 import('bootstrap');
 
-// TEST: Trigger build 2
+// TEST: Trigger build 3
 
 // NOTE: This is fix for "All files must be modules when the '--isolatedModules' flag is provided." error.
 export {};
