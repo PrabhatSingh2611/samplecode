9abcce099b95a5dc360fc322a945296c77eb0acb
rshwm <roman.shtepa@windmill.ch>
2022-05-24 19:17:25 +0200

    [VA-230]: me query with mocked user

--- backend/src/main/java/digital/windmill/audra/graphql/resolver/employee/EmployeeResolver.java
+++ backend/src/main/java/digital/windmill/audra/graphql/resolver/employee/EmployeeResolver.java
@@ -3,8 +3,10 @@ package digital.windmill.audra.graphql.resolver.employee;
 import digital.windmill.audra.graphql.facade.EmployeeFacade;
 import digital.windmill.audra.graphql.type.ConnectionPayload;
 import digital.windmill.audra.graphql.type.Employee;
+import digital.windmill.audra.graphql.type.EmployeePayload;
 import digital.windmill.audra.graphql.type.input.EmployeesInput;
 import digital.windmill.audra.graphql.utils.ConnectionUtils;
+import digital.windmill.audra.service.impl.SecurityService;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -13,9 +15,14 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class EmployeeResolver implements GraphQLQueryResolver {
 
-    private EmployeeFacade employeeFacade;
+    private final EmployeeFacade employeeFacade;
+    private final SecurityService securityService;
 
     public ConnectionPayload<Employee> employees(EmployeesInput input) {
         return ConnectionUtils.buildPayload(employeeFacade.getEmployees(input));
+    }
+
+    public EmployeePayload me() {
+        return new EmployeePayload(employeeFacade.findEmployeeByUuid(securityService.getCurrentUserUuid()));
     }
 }
