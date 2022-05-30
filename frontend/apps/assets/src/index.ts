11d25fd8ad0e0a5329c0e8d8dbe7239dc0dd4c46
Ivan Myronov <gmironovivan1992@gmail.com>
2022-05-30 20:11:49 +0300

    [VA] Trigger CI/CCD

--- frontend/apps/assets/src/index.ts
+++ frontend/apps/assets/src/index.ts
@@ -1,6 +1,6 @@
 import('./bootstrap');
 
-// TEST: Trigger Assets build !
+// TEST: Trigger Assets build
 
 // NOTE: This is fix for "All files must be modules when the '--isolatedModules' flag is provided." error.
 export {};
--- frontend/apps/people/src/index.ts
+++ frontend/apps/people/src/index.ts
@@ -1,6 +1,6 @@
 import('bootstrap');
 
-// TEST: Trigger People build !
+// TEST: Trigger People build
 
 // NOTE: This is fix for "All files must be modules when the '--isolatedModules' flag is provided." error.
 export {};
