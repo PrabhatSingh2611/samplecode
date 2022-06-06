66c169611d1bc495ac5766cac97e2d64dba63950
Serhii Shamrikov <serhii.shamrikov@windmill.digital>
2022-06-06 14:46:29 +0000

    Merged in task/VA-341-fix-assets-list-schema-fix-icon-for-asset-type (pull request #175)
    [VA-341] fix styles for asset type
    
    * VA-341 Fix assets schema. Fix icon for assets type
    
    * Merge branch 'develop' of bitbucket.org:windmillgmbh/audra-platform into task/VA-341-fix-assets-list-schema-fix-icon-for-asset-type
    
     Conflicts:
    	frontend/packages/wdx/src/components/icon/icons.config.ts
    	frontend/packages/wdx/src/components/icon/icons.…
    * VA-341 Some fix
    
    * [VA-341] styled assetTableRow
    
    * Merge branch 'task/VA-341-fix-assets-list-schema-fix-icon-for-asset-type' of bitbucket.org:windmillgmbh/audra-platform into task/VA-341-fix-assets-list-schema-fix-icon-for-asset-type
    
    * [VA-341] fix unused script
    
    * Merge branch 'develop' of bitbucket.org:windmillgmbh/audra-platform into task/VA-341-fix-assets-list-schema-fix-icon-for-asset-type
    
    * [VA-341] fix lint
    
    
    Approved-by: Volodymyr Zaloznyi
    Approved-by: Andriy Uvarov
    Approved-by: Alexander Molchanov
    Approved-by: Yevhenii Dryha

--- frontend/apps/assets/src/features/assets-table/components/table/AssetTableRow.tsx
+++ frontend/apps/assets/src/features/assets-table/components/table/AssetTableRow.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { AssigneeCell } from 'features/assets-table/components/table/table-cells/AssigneeCell';
+import { LocationCell } from 'features/assets-table/components/table/table-cells/LocationCell';
+import { MoreMenuCell } from 'features/assets-table/components/table/table-cells/MoreMenuCell';
+import { AssetsItemFragment } from 'features/assets-table/graphql/queries/assetsForAssetsList.generated';
 import {
     WBox,
     WTable,
@@ -10,13 +14,9 @@ import {
     WPill,
     deleteUtcFromDate,
     WIcon,
+    useWTheme,
     TIcons,
 } from 'wdx';
-
-import { AssigneeCell } from 'features/assets-table/components/table/table-cells/AssigneeCell';
-import { LocationCell } from 'features/assets-table/components/table/table-cells/LocationCell';
-import { MoreMenuCell } from 'features/assets-table/components/table/table-cells/MoreMenuCell';
-import { AssetsItemFragment } from 'features/assets-table/graphql/queries/assetsForAssetsList.generated';
 
 interface IAssetTableRow {
     row: AssetsItemFragment;
@@ -29,14 +29,23 @@ export default function AssetTableRow({
     onEditRow,
     onDeleteRow,
 }: IAssetTableRow): JSX.Element {
+    const theme = useWTheme();
     const { title, assignee, location, serialNumber, tagNumber, type, waybillDate } = row;
     const waybillDateString = deleteUtcFromDate(waybillDate);
 
     return (
         <WTable.Row hover>
             <WTable.Cell align={AlignTableCell.LEFT}>
-                <WAvatar variant="rounded" sx={{ width: 64, height: 64, bgcolor: '#D6E4FF' }}>
-                    <WIcon name={type.iconName as TIcons} sx={{ height: 30, color: 'black' }} />
+                <WAvatar
+                    variant="rounded"
+                    sizes="64"
+                    sx={{ bgcolor: theme.palette.secondary.lighter }}
+                >
+                    <WIcon
+                        name={type.iconName as TIcons}
+                        fontSize="medium"
+                        sx={{ color: theme.palette.common.black }}
+                    />
                 </WAvatar>
             </WTable.Cell>
 
