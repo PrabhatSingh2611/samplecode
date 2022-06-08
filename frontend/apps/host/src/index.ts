499e026dd2b7210b47a027c3ae478f9ce6c88778
Ivan Myronov <gmironovivan1992@gmail.com>
2022-06-08 12:47:24 +0300

    [VA] Trigger CI/CD

--- frontend/apps/host/src/index.ts
+++ frontend/apps/host/src/index.ts
@@ -1,6 +1,6 @@
 import('bootstrap');
 
-// TEST: Trigger Host build
+// TEST: Trigger Host build !
 
 // NOTE: This is fix for "All files must be modules when the '--isolatedModules' flag is provided." error.
 export {};
